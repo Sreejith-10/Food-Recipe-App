ab32a43a995f0772c80f28c8807bfc5f876346e2
Sreejith <132558235+Sreejith-10@users.noreply.github.com>
2023-08-21 20:52:33 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,19 @@
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
-import Main from "./components/Main/Main";
 import "./App.css";
 import Menu from "./components/Menu/Menu";
-import { BrowserRouter } from "react-router-dom";
 
 function App() {
 	return (
-		<BrowserRouter>
-			<div className='food-page'>
-				<div className='page-header'>
-					<Header />
-				</div>
-				<div className='page-content'>
-					<Menu />
-				</div>
-				<div className='page-footer'>
-					<Footer />
-				</div>
+		<div className='food-page'>
+			<div className='page-header'></div>
+			<div className='page-content'>
+				<Menu />
 			</div>
-		</BrowserRouter>
+			<div className='page-footer'>
+				<Footer />
+			</div>
+		</div>
 	);
 }
 
