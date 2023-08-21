810dd467bf9a9ecce0479cd6e6212b2ac3087a78
Sreejith <132558235+Sreejith-10@users.noreply.github.com>
2023-08-21 10:05:35 +0530

    js commit

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -3,7 +3,7 @@ import "./Main.css";
 export default function Main() {
 	return (
 		<section className="main-section">
-			<img className="hero-image" src="/public/images/hero.png" alt="" />
+			<img className="hero-image" src="" alt="" />
 			<h1 className="hero-h1">welcome</h1>
 			<p className="hero-description">
 				Your ultimatie source for a comprehensive list of food ingredients. Find
